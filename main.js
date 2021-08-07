ccc077f4107d555e23ccd50354ecb1a7fdd41206
valenf1999 <valentinfantile@gmail.com>
2021-08-07 19:03:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//camelCase
+
+let data = 0;
+
+document.getElementById("counting").innerText = data;
+
+function increment() {
+  data = data + 1;
+  document.getElementById("counting").innerText = data;
+}
+
+function decrement() {
+  data = data - 1;
+  document.getElementById("counting").innerText = data;
+}
+
+function reset() {
+  data = 0
+  document.getElementById("counting").innerText = data;
+}
